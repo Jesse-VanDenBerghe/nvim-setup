225ffaf771f22c0194b37f2e95df50f157d2cd64
Jesse Van Den Berghe <jesse.van.den.berghe@persgroep.net>
2026-01-13 16:02:31 +0100

    Add full push macro

--- lua/jesse/plugins/fugitive.lua
+++ lua/jesse/plugins/fugitive.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<leader>gg", vim.cmd.Git, {desc = "Open Fugitive Git Status"})
-vim.keymap.set("n", "<leader>ga", ":!git add -A", {desc = "Git Stage"})
+vim.keymap.set("n", "<leader>ga", ":!git add -A<CR>", {desc = "Git Stage"})
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {desc = "Git Push"})
 
 local function gc_commit()
   local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
-  local ticket = branch:match("^([A-Z]+-%d+)")
+  local ticket = branch:match("([A-Z]+-%d+)")
 
   vim.ui.input({ prompt = "Commit message: " }, function(input)
     if input then
@@ -14,4 +14,11 @@ local function gc_commit()
   end)
 end
 
+local function gcp_commit_and_push()
+    vim.fn.system("git add -A")
+    gc_commit()
+    vim.cmd("Git push")
+end
+
 vim.keymap.set("n", "<leader>gc", gc_commit, {desc = "Commit with Jira ticket prefix"})
+vim.keymap.set("n", "<leader>gf", gcp_commit_and_push, {desc = "Commit and Push with Jira ticket prefix"})
