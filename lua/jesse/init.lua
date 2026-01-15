beeaa1ec5f759352ebd20cd6e5f6a559f2600654
Jesse Van Den Berghe <jesse.van.den.berghe@persgroep.net>
2026-01-15 09:25:20 +0100

    adding some stuff from kickstart.nvim before lazy.nvim package manager changes

--- lua/jesse/init.lua
+++ lua/jesse/init.lua
@@ -1,5 +1,6 @@
 print("Setting up Profile: Jesse")
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 print("\nMaking sure plugins are up to date...")
 require("jesse.pack")
@@ -28,6 +29,16 @@ vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+
+vim.opt.breakindent = true
+
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.o.inccommand = 'split'
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -45,6 +56,8 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+vim.o.cursorline = true
+
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
@@ -53,13 +66,36 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "120"
 
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 vim.diagnostic.config({
   virtual_text = true,
   virtual_lines = { current_line = true },
 })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   pattern = { "*" },
   command = "silent! wall",
   nested = true,
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
 })
--- lua/jesse/remap.lua
+++ lua/jesse/remap.lua
@@ -1,5 +1,4 @@
 print("- Remaps set")
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.api.nvim_set_keymap('t', '<C-t>', '<C-\\><C-n>', {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
