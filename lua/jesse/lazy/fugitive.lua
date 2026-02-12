72bc585b2403a010892dc05ec213446ee7fb4aed
Jesse Van Den Berghe <jesse.van.den.berghe@persgroep.net>
2026-02-12 10:35:03 +0100

    add git new branch on default

--- lua/jesse/lazy/fugitive.lua
+++ lua/jesse/lazy/fugitive.lua
@@ -2,10 +2,6 @@ return {
 	{
 		"tpope/vim-fugitive",
 		config = function()
-			vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Git" })
-			vim.keymap.set("n", "<leader>ga", ":!git add -A<CR>", { desc = "Git [A]dd" })
-			vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Git [P]ush" })
-
 			local function gc_commit()
 				local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
 				local ticket = branch:match("([A-Z]+-%d+)")
@@ -18,22 +14,41 @@ return {
 				end)
 			end
 
-	local function gcp_commit_and_push()
-		vim.fn.system("git add -A")
-		local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
-		local ticket = branch:match("([A-Z]+-%d+)")
+			local function gcp_commit_and_push()
+				vim.fn.system("git add -A")
+				local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
+				local ticket = branch:match("([A-Z]+-%d+)")
 
-		vim.ui.input({ prompt = "Commit message: " }, function(input)
-			if input then
-				local message = ticket and (ticket .. ": " .. input) or input
-				vim.cmd("Git commit -m '" .. message:gsub("'", "'\\''") .. "'")
-				vim.cmd("Git push")
+				vim.ui.input({ prompt = "Commit message: " }, function(input)
+					if input then
+						local message = ticket and (ticket .. ": " .. input) or input
+						vim.cmd("Git commit -m '" .. message:gsub("'", "'\\''") .. "'")
+						vim.cmd("Git push")
+					end
+				end)
 			end
-		end)
-	end
+
+			local function gbnd_new_branch_on_default()
+				vim.ui.input({ prompt = "New branch name" }, function(input)
+					if input then
+						local default_branch = vim.fn.systemlist(
+							"git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'"
+						)[1]
+						vim.cmd("Git checkout " .. default_branch)
+						vim.cmd("Git pull")
+						vim.cmd("Git checkout -b " .. input)
+					end
+				end)
+			end
+
+			vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Git" })
+			vim.keymap.set("n", "<leader>ga", ":!git add -A<CR>", { desc = "Git [A]dd" })
+			vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Git [P]ush" })
 
 			vim.keymap.set("n", "<leader>gcm", gc_commit, { desc = "Git Commit with [M]essage" })
 			vim.keymap.set("n", "<leader>gcp", gcp_commit_and_push, { desc = "Git Commit and [P]ush" })
+
+			vim.keymap.set("n", "<leader>gbnd", gbnd_new_branch_on_default, { desc = "New Branch on Default" })
 
 			-- Git Finders
 			local builtin = require("telescope.builtin")
