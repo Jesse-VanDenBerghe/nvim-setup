37d5a1f716b40ed8968d41c9c0b3c82cd4d82859
Jesse Van Den Berghe <jesse.van.den.berghe@persgroep.net>
2026-02-19 16:59:36 +0100

    snacks dashboard

--- lua/jesse/lazy/snacks.lua
+++ lua/jesse/lazy/snacks.lua
@@ -1,8 +1,15 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	opts = {
-		input = { enabled = true },
-	},
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        input = { enabled = true },
+        dashboard = {
+            sections = {
+                { section = "header" },
+                { section = "keys", gap = 1 },
+                { section = "startup" },
+            },
+        },
+    },
 }
