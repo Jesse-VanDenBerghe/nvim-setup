a8a00c4c70055cc99c1dc14a8d70c48bffb3aa2d
Jesse Van Den Berghe <jesse.van.den.berghe@persgroep.net>
2026-02-23 20:33:28 +0100

    cleanup elixir setup

--- lua/jesse/languages/elixir.lua
+++ lua/jesse/languages/elixir.lua
@@ -3,60 +3,47 @@ local run_notify = util.run_notify
 
 local elixirgroup = vim.api.nvim_create_augroup("ElixirTools", { clear = true })
 
+local function setup_elixir()
+	-- Build keymaps
+	vim.keymap.set("n", "<leader>bp", function()
+		run_notify({ "mix", "compile" }, "mix compile")
+	end, { desc = "[B]uild [P]roject (mix compile)" })
+
+	-- Run keymaps
+	vim.keymap.set("n", "<leader>rf", function()
+		run_notify({ "mix", "run", vim.fn.expand("%") }, "mix run")
+	end, { desc = "[R]un [F]ile (mix run)" })
+
+	-- Test keymaps
+	vim.keymap.set("n", "<leader>tp", function()
+		run_notify({ "mix", "test" }, "mix test")
+	end, { desc = "[T]est [P]roject (mix test)" })
+
+	vim.keymap.set("n", "<leader>tf", function()
+		run_notify({ "mix", "test", vim.fn.expand("%") }, "mix test file")
+	end, { desc = "[T]est [F]ile (mix test)" })
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+	group = elixirgroup,
+	callback = function()
+		local cwd = vim.fn.getcwd()
+		if vim.fn.filereadable(cwd .. "/mix.exs") == 1 then
+			setup_elixir()
+		end
+	end,
+})
+
+-- .exs-only: run as a standalone script (buffer-local, only relevant for script files)
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "elixir",
 	group = elixirgroup,
 	callback = function()
 		local filename = vim.fn.expand("%:t")
-
-		-- Build keymaps
-		vim.keymap.set(
-			"n",
-			"<leader>bp",
-			function()
-				run_notify({ "mix", "compile" }, "mix compile")
-			end,
-			{ buffer = true, desc = "Build project (mix compile)" }
-		)
-
-		-- Run keymaps
-		vim.keymap.set(
-			"n",
-			"<leader>rf",
-			function()
-				run_notify({ "mix", "run", vim.fn.expand("%") }, "mix run")
-			end,
-			{ buffer = true, desc = "Run file (mix run)" }
-		)
-
 		if filename:match("%.exs$") then
-			vim.keymap.set(
-				"n",
-				"<leader>rs",
-				function()
-					run_notify({ "elixir", vim.fn.expand("%") }, "elixir script")
-				end,
-				{ buffer = true, desc = "Run script (elixir)" }
-			)
+			vim.keymap.set("n", "<leader>rs", function()
+				run_notify({ "elixir", vim.fn.expand("%") }, "elixir script")
+			end, { buffer = true, desc = "[R]un [S]cript (elixir)" })
 		end
-
-		-- Test keymaps
-		vim.keymap.set(
-			"n",
-			"<leader>tp",
-			function()
-				run_notify({ "mix", "test" }, "mix test")
-			end,
-			{ buffer = true, desc = "Test project (mix test)" }
-		)
-
-		vim.keymap.set(
-			"n",
-			"<leader>tf",
-			function()
-				run_notify({ "mix", "test", vim.fn.expand("%") }, "mix test file")
-			end,
-			{ buffer = true, desc = "Test file (mix test)" }
-		)
 	end,
 })
--- lua/jesse/languages/gradle.lua
+++ lua/jesse/languages/gradle.lua
@@ -1,16 +1,16 @@
 local gradlegroup = vim.api.nvim_create_augroup("GradleTools", { clear = true })
 local gradle_picker = require("jesse.languages.gradle.picker")
 
-local function update_gradle_setup()
-	local cwd = vim.fn.getcwd()
-	if vim.fn.filereadable(cwd .. "/gradlew") == 1 then
-		vim.keymap.set("n", "<leader>sg", gradle_picker.open, { desc = "Search Gradle task" })
-	else
-		pcall(vim.keymap.del, "n", "<leader>fg")
-	end
+local function setup_gradle()
+	vim.keymap.set("n", "<leader>sg", gradle_picker.open, { desc = "Search Gradle task" })
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
 	group = gradlegroup,
-	callback = update_gradle_setup,
+	callback = function()
+		local cwd = vim.fn.getcwd()
+		if vim.fn.filereadable(cwd .. "/gradlew") == 1 then
+			setup_gradle()
+		end
+	end,
 })
